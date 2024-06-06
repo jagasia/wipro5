32ba8fe0cdbb16d38c4ed848eb0bb88b0b393ee6
jagasia <rjagadeeswaran@yahoo.com>
2024-06-06 14:26:59 +0530

    06Jun 14:26

--- java/06-jun-1/src/App.java
+++ java/06-jun-1/src/App.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-
+		Scanner sc=new Scanner(System.in);
+		int i=sc.nextInt();
+		System.out.println("You entered "+i);
+	
 	}
 
 }
