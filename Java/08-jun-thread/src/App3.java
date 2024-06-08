f32126b0cf04a9a04b3c23b3b70192d92f348078
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-06-08 17:20:09 +0530

    08Jun 17:20

--- Java/08-jun-thread/src/App3.java
+++ Java/08-jun-thread/src/App3.java
@@ -1,12 +1,11 @@
-class MyThread extends Thread
-{
+class MyThread extends Thread {
 	Maths maths;
-	public MyThread(Maths m)
-	{
-		maths=m;
+
+	public MyThread(Maths m) {
+		maths = m;
 	}
-	public void run()
-	{
+
+	public void run() {
 		try {
 			maths.displayEven();
 		} catch (InterruptedException e) {
@@ -16,37 +15,33 @@ class MyThread extends Thread
 	}
 }
 
-class Maths
-{	
-	public void displayEven() throws InterruptedException
-	{
-		System.out.println(Thread.currentThread().getName()+" entered displayEven method now");
-		for(int i=0;i<3;i++)
-		{
+class Maths {
+	public void displayEven() throws InterruptedException {
+		System.out.println(Thread.currentThread().getName() + " entered displayEven method now");
+		for (int i = 0; i < 3; i++) {
 			Thread.sleep(1000);
-			System.out.println(Thread.currentThread().getName()+":"+i);
+			System.out.println(Thread.currentThread().getName() + ":" + i);
 		}
-	
+
 		synchronized (this) {
-			System.out.println(Thread.currentThread().getName()+" entered sync block now");
-			for(int i=2;i<20;i+=2)
-			{
+			System.out.println(Thread.currentThread().getName() + " entered sync block now");
+			for (int i = 2; i < 20; i += 2) {
 				Thread.sleep(1000);
-				System.out.println(Thread.currentThread().getName()+":"+i);
+				System.out.println(Thread.currentThread().getName() + ":" + i);
 			}
 		}
-		
-		System.out.println(Thread.currentThread().getName()+" exited displayEven method now");
+
+		System.out.println(Thread.currentThread().getName() + " exited displayEven method now");
 	}
 }
 
 public class App3 {
 
 	public static void main(String[] args) {
-		Maths m=new Maths();
-		MyThread t1=new MyThread(m);
-		MyThread t2=new MyThread(m);
-		
+		Maths m = new Maths();
+		MyThread t1 = new MyThread(m);
+		MyThread t2 = new MyThread(m);
+
 		t1.setName("Thread 1");
 		t2.setName("Thread 2");
 		t1.start();
