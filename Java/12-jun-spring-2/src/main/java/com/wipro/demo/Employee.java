2cd5c06821d0a40390ab29a95792e15494db9456
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-06-12 15:28:27 +0530

    12Jun 15:28

---
+++ Java/12-jun-spring-2/src/main/java/com/wipro/demo/Employee.java
@@ -0,0 +1,56 @@
+package com.wipro.demo;
+
+public class Employee {
+	private Integer id;
+	private String name;
+	private Department department;
+	private Integer salary;
+	
+	public Employee() {}
+
+	public Employee(Integer id, String name, Department department, Integer salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.department = department;
+		this.salary = salary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", department=" + department + ", salary=" + salary + "]";
+	}
+	
+}
