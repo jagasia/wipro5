443932e72da43683a40e4514a5997dad92f86655
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-06-12 10:36:01 +0530

    12Jun 10:35

---
+++ Java/12-jun-mvc-1/src/main/java/com/wipro/demo/controller/HomeController.java
@@ -0,0 +1,46 @@
+package com.wipro.demo.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HomeController
+ */
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path=request.getRequestURI();
+		PrintWriter out = response.getWriter();
+		out.print("Message from home controller:<br/>");
+		response.getWriter().print(path);
+		String[] arr = path.split("/");
+		String url=arr[arr.length-1];
+		response.sendRedirect(url+".jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
