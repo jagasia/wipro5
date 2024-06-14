24a26f5131d7bd3f79c3578ff96ae0f6a05797b1
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-06-14 09:35:38 +0530

    14-Jun 09:35

--- Spring/13-jun-mvc-1/src/main/java/com/wipro/demo/controller/MyController.java
+++ Spring/13-jun-mvc-1/src/main/java/com/wipro/demo/controller/MyController.java
@@ -2,11 +2,13 @@ package com.wipro.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
 	
 	@GetMapping("/home")
+//	@ResponseBody
 	public String home()
 	{
 		return "index";
