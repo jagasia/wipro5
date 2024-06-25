bbfc17555a9628e832850045c5cd66bc84e09de4
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-06-25 10:49:29 +0530

    25Jun 10:49

---
+++ microservices/customer-service/src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@GetMapping
+	public String home()
+	{
+		return "Customer service is running";
+	}
+}
