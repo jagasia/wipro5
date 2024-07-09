6e652b3c150128b02d3c2cee5d63b745f6904bc7
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-07-09 16:26:23 +0530

    09Jul 16:26

---
+++ js/three.js
@@ -0,0 +1,5 @@
+var i=20;
+var i=30;
+let j=20;
+let j=30;
+console.log(i);
---
+++ js/two.js
@@ -0,0 +1,7 @@
+var arr=["India","Australia","Sri lanka","Pakistan"];
+arr.sort();
+arr.reverse();
+for(var x of arr)
+{
+    console.log(x);
+}
