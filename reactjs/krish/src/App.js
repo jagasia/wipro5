8079e141d575db98da4432aaa83485ee4f905ece
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-07-11 14:17:08 +0530

    11Jul 14:17

--- reactjs/krish/src/App.js
+++ reactjs/krish/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import Hello from './components/hello';
 
 function App() {
   return (
     <div className="App">
-      hi
+    <div class="container-fluid p-5 bg-primary text-white text-center">
+  <h1>My First ReactJs App</h1>
+  <p>created using visual studio code!</p> 
+</div>
+  
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <Hello />
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>        
+      
+    </div>
+  </div>
+</div>  
     </div>
     
   );
---
+++ reactjs/krish/src/components/hello.js
@@ -0,0 +1,11 @@
+const { Component } = require("react");
+
+class Hello extends Component
+{
+    render(){
+        return <div>
+            Hello All.....
+        </div>
+    }
+}
+export default Hello;
