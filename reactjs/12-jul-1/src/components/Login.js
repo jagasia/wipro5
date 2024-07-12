697dedc0b3aa1ae9b44e17db2827b7b23e2ca2a8
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-07-12 11:57:32 +0530

    12Jul 11:57

--- reactjs/12-jul-1/src/components/Login.js
+++ reactjs/12-jul-1/src/components/Login.js
@@ -5,9 +5,10 @@ class Login extends Component
     constructor(props)
     {
         super(props);
-        this.state={username:'dummy',password:'dummy',status:''};
+        this.state={username:'dummy',password:'dummy',status:'', uvalid:true, pvalid:true};
         this.fnUpdate=this.fnUpdate.bind(this);
         this.fnLogin=this.fnLogin.bind(this);
+        this.fnValidate=this.fnValidate.bind(this);
     }
 
     fnLogin()
@@ -22,15 +23,35 @@ class Login extends Component
         }
     }
 
+    fnValidate()
+    {
+        // alert('validating')
+        //validate data. username must be atleast 3 chars
+        if(this.state.username.length<3)
+            {
+                    this.setState({uvalid:false})
+                    // alert('invalid')
+            }else
+            this.setState({uvalid:true})
+    }
     fnUpdate(event)
     {
+        
         // alert(event.target.name)
         this.setState({[event.target.name]:event.target.value})
+        this.fnValidate();
     }
 
     render(){
+        let class1='form-control';
+        if(this.state.uvalid==false)
+        {
+            class1+=" is-invalid"
+        }
         return <div>
-            Username:<input type="text" name="username" className="form-control" onChange={this.fnUpdate} />
+            Username:<input type="text" name="username" className={class1} onKeyUp={this.fnUpdate} onBlur={this.fnValidate} onChange={this.fnValidate}  />
+            { !this.state.uvalid && <p style={{color:'red'}}>Username is invalid</p>}
+            
             Password:<input type="password" name="password" className="form-control" onChange={this.fnUpdate} />
             <br/>
             <input type="button" value={'Login'} className="btn btn-info" onClick={this.fnLogin} />
