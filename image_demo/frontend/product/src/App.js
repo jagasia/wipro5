07b07838928af91cd59ff6744983811a7e7fbcc7
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-07-19 16:53:28 +0530

    19Jul 16:53

--- image_demo/frontend/product/src/App.js
+++ image_demo/frontend/product/src/App.js
@@ -1,12 +1,22 @@
+import { useEffect } from 'react';
 import './App.css';
 import ProductForm from './components/ProductForm';
 import ProductGrid from './components/ProductGrid';
 
-function App() {
+function App() {  
+  var x=20;
+  useEffect(()=>{
+    
+
+  });
   return (
     <div className="App">
       <div className='p-5 bg-info text-white text-center'>
         <h2>Product Image Demo</h2>
+        <h3>You are accessing this portal from</h3>
+        <h4>{
+          x
+          }</h4>
       </div>
       <div className='row'>
         <div className='col-sm-4'>1</div>
